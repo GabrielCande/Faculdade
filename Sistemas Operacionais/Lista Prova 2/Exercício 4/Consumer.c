812b6f9679caf8b8e063c04d4770765887ee8e3f
Gabriel Candelária <80334373+GabrielCande@users.noreply.github.com>
2022-11-16 21:23:52 -0300

    Create Consumer.c

---
+++ Sistemas Operacionais/Lista Prova 2/Exercício 4/Consumer.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <pthread.h>
+
+
+#define SHM_SIZE 1024
+
+const int REP = 5;
+char dado;
+key_t key;
+int shmid,flagsid;
+char *data;
+int *flags;
+int in;
+
+void exit(int signum){
+  shmdt(data);                              //segmento 3
+  shmctl(shmid, IPC_RMID, NULL);            //
+
+  shmdt(flags);                             //segmento 4
+  shmctl(flagsid, IPC_RMID, NULL);        //
+  exit(0);
+}
+
+void consumer(){
+  printf("Consumer was born!\n");
+  while(1){
+    while(!flags[3]);
+    flags[1] = 1;
+    while(flags[0] && (!flags[2]));
+    dado = data[in];
+    data[in] = ' ';
+    printf("Consumed... %c \n", dado);
+    in = ++in % REP;
+    flags[3]--;
+    flags[2] = 0;
+    flags[1] = 0;
+  }
+}
+
+void main(){
+  key = ftok("/home", 'A');                      //  Segmento
+  shmid = shmget(key, 1024, 0644 | IPC_CREAT);   //
+  data = (malloc(5*sizeof(char)));               //     1
+  data = shmat(shmid, (void *)0, 0);             //
+
+  //Peterson
+  key = ftok("/home/downloads", 'B');            //  Segmento
+  flagsid = shmget(key, 1024, 0644 | IPC_CREAT); //
+  flags = (malloc(1*sizeof(int)));               //     2
+  flags = shmat(flagsid, (void *)0, 0);          //
+
+
+  printf("The Producer x Consumer Problem\n");
+  int status;
+  in = 0;
+  
+  signal(SIGINT, exit);
+  consumer();
+}
